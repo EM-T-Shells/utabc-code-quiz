a1b0b7a98307d8f545fd250f5910573f401628e0
Gokhan Emre Taskale <taskaleg@gmail.com>
2022-10-11 12:14:42 -0500

    added hof and quiz html files into index. will manipulate  boostrap display properties via DOM - javascript

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,11 @@ const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const score = document.getElementById("scoreContainer");
 
+//readyStartBtn to quiz page
+readyStartBtn.onclick = () =>{
+  location.assign(url: "http://localhost)
+}
+
 let questionBank = [
   {
     question: "What does HTML stand for?",
@@ -53,96 +58,3 @@ let questionBank = [
   }
 ];
 
-//declaring what is the last and first question
-const lastQuestion = questions.length - 1;
-let currentQuestion = 0;
-let count = 0;
-const questionTime = 30; //30 seconds
-const gaugeWidth = 150; //px
-const gaugeUnit = gaugeWidth / questionTime;
-let TIMER;
-score = 0;
-
-//retrieve questions and choices
-function nextQuestion() {
-  let question = questionBank[currentQuestion];
-
-  questionText.innerHTML = question.questionText;
-  choiceA.innerHTML = question.choiceA;
-  choiceB.innerHTML = question.choiceB;
-  choiceC.innerHTML = question.choiceC;
-  choiceD.innerHTML = question.choiceD;
-}
-
-
-readyStartBtn.addEventListener("click", startQuiz);
-
-function startQuiz(){
-  nextQuestion();
-  progress();
-  countDown();
-  TIMER = setInterval(countDown, 1000);
-}
-
-
-//updating progress
-function progress() {
-  for(let questionIndex = 0; questionIndex <= lastQuestion; questionIndex++){
-    progress.innerHTML += "<div class='prog' id="+ questionIndex +"></div>";
-  }
-}
-
-function countDown(){
-  if(count <= questionTime){
-    counter.innerHTML = count;
-    timeGauge.style.width = count * gaugeUnit + "px";
-    count++
-  } else{
-    count=0;
-    answerIsWrong();
-    }
-  if (currentQuestion < lastQuestion){
-    currentQuestion++;
-    nextQuestion();
-  } else {
-      clearInterval(TIMER);
-      updateScore();
-    }
-}
-
-function checkAnswer(answer){
-  if(answer == questions[currentQuestion].correct){
-      // answer is correct
-      score++;
-      // change progress color to green
-      answerIsCorrect();
-  }else{
-      // answer is wrong
-      // change progress color to red
-      answerIsWrong();
-  }
-  count = 0;
-  if(currentQuestion < lastQuestion){
-      currentQuestion++;
-      nextQuestion();
-  }else{
-      // end the quiz and show the score
-      clearInterval(TIMER);
-      updateScore();
-  }
-}
-
-// answer is correct
-function answerIsCorrect(){
-  document.getElementById(runningQuestion).style.backgroundColor = "#0f0";
-}
-
-// answer is Wrong
-function answerIsWrong(){
-  document.getElementById(runningQuestion).style.backgroundColor = "#f00";
-}
-
-// score render
-function updateScore(){
-  const scorePerCent = Math.round(100 * score/questions.length);
-}
